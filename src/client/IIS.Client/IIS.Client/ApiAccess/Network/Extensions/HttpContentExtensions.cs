f9fcbef294dfa40c8eb3328a0bd53912e9c0a1d3
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-02-28 23:09:33 +0100

    Case insensitive JSON deserialization

--- src/client/IIS.Client/IIS.Client/ApiAccess/Network/Extensions/HttpContentExtensions.cs
+++ src/client/IIS.Client/IIS.Client/ApiAccess/Network/Extensions/HttpContentExtensions.cs
@@ -4,9 +4,14 @@ namespace IIS.Client.ApiAccess.Network.Extensions;
 
 internal static class HttpContentExtensions
 {
+    private static readonly JsonSerializerOptions _options = new()
+    {
+        PropertyNameCaseInsensitive = true,
+    };
+
     public static T? ReadFromJson<T>(this HttpContent content)
     {
         using Stream stream = content.ReadAsStream();
-        return JsonSerializer.Deserialize<T>(stream);
+        return JsonSerializer.Deserialize<T>(stream, _options);
     }
 }
