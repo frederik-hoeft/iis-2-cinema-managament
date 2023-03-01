ea9c42e8c110ea82a921a4f729c53c4b402f32b0
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-03-01 17:14:24 +0100

    start integrating DB stuff into API
    Co-authored-by: Ben <78792287+benbekir@users.noreply.github.com>

--- src/server/java/Server/Server-Api/src/main/java/IIS/Server/api/management/movie/responses/GetMoviesResponse.java
+++ src/server/java/Server/Server-Api/src/main/java/IIS/Server/api/management/movie/responses/GetMoviesResponse.java
@@ -1,8 +1,11 @@
 package IIS.Server.api.management.movie.responses;
 
+import java.util.Collection;
+
 import lombok.Data;
 
 @Data
 public class GetMoviesResponse {
     boolean success;
+    Collection<GetMoviesResponseEntry> movies;
 }
