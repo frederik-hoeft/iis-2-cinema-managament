3462a4290c4c8e4aa46d5a166e7cd047f0b105d6
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-03-07 19:29:30 +0100

    better exception message in client

--- src/client/IIS.Client/IIS.Client/ApiAccess/ModelValidation/ApiResponseExtensions.cs
+++ src/client/IIS.Client/IIS.Client/ApiAccess/ModelValidation/ApiResponseExtensions.cs
@@ -9,7 +9,6 @@ internal static class ApiResponseExtensions
             return response;
         }
         string reason = response is null ? $"Response of type '{typeof(T).Name}' was null" : $"{typeof(T).Name}.Success was false";
-        string? error = response?.Error ?? "No details could be read. The cause is probably an internal server error. Go fix your code.";
-        throw new ApiResponseValidationException($"Failed to perform the requested operation. Reason: \"{reason}\". Details: \"{error}\"");
+        throw new ApiResponseValidationException($"Failed to perform the requested operation. Reason: \"{response?.Error ?? reason}\".");
     }
 }
